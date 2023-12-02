24c400a54b2506b528ecb8e5dbd95abbee99c7ac
bloodline4a <pauloroyhernandez@gmail.com>
2023-12-02 09:04:57 +0800

    all sync

---
+++ coreapp/models.py
@@ -0,0 +1,117 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import EmailValidator
+from django.contrib.auth.hashers import make_password
+from django.utils.translation import gettext as _
+from django.contrib.auth.models import User
+from django.db import transaction
+from django.utils import timezone
+
+class Admin(models.Model):
+    uname = models.CharField(max_length=255)
+    pword = models.CharField(max_length=255)
+    def __str__(self):
+        return self.uname
+    
+    
+class Issues(models.Model):
+    name = models.CharField(max_length=255)
+    issue = models.CharField(max_length=255)
+    solution = models.CharField(max_length=255)
+
+class Units(models.Model):
+    unit_type = models.CharField(max_length=255)
+    unit_blt = models.CharField(max_length=255, default='location')
+    unt_price = models.FloatField()
+    unt_availability = models.BooleanField(default=True) 
+
+    def availability_display(self):
+        return "Vacant" if self.unt_availability else "Occupied"
+    
+    def __str__(self):
+        return self.unit_type
+
+class Booked(models.Model):
+    APPROVAL_CHOICES = (
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('declined', 'Declined'),
+    )
+    bookt = models.CharField(max_length=255,default='duration')
+    name = models.CharField(max_length=255)
+    unit = models.CharField(max_length=255)
+    pnum = models.CharField(max_length=20)  
+    date = models.DateField(max_length=255)
+    emel = models.CharField(max_length=255, validators=[EmailValidator()], default='custom@example.com')    
+    approval_status = models.CharField(max_length=20, choices=APPROVAL_CHOICES, default='Pending')
+
+class Payment(models.Model):
+    name = models.CharField(max_length=255)
+    amount = models.IntegerField()
+    ref = models.IntegerField()
+    mop = models.CharField(max_length=255)  
+    unit = models.CharField(max_length=255)
+    date = models.DateField(default=timezone.now)
+    tenant = models.ForeignKey('Tenants', on_delete=models.CASCADE)  
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ('pending', 'Pending'),
+            ('successful', 'Successful'),
+            ('decline', 'Decline')
+        ],
+        default='Pending'
+    )
+    
+class Tenants(AbstractUser):
+    tent_name = models.CharField(max_length=255)
+    tent_uname = models.CharField(max_length=255)
+    unit_type = models.CharField(max_length=255)
+    tent_pnum = models.CharField(max_length=20)  
+    tent_emel = models.EmailField(max_length=255, validators=[EmailValidator()], default='custom@example.com')
+    tent_pword = models.CharField(max_length=255)
+    assigned_unit = models.ForeignKey(Units, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    @transaction.atomic
+    def save(self, *args, **kwargs):
+        if not self.pk:  # Check if the instance is being created (not updated)
+            # Check if there is an available unit of the specified type
+            booked_unit = Units.objects.filter(unit_type=self.unit_type, unt_availability=True).first()
+            if booked_unit:
+                # Check if the unit is not already assigned to another tenant
+                if not Tenants.objects.filter(assigned_unit=booked_unit).exists():
+                    # If the tenant already has an assigned unit, reset its availability to 'True'
+                    if self.assigned_unit and self.assigned_unit.unt_availability == False:
+                        self.assigned_unit.unt_availability = True
+                        self.assigned_unit.save()
+                    booked_unit.unt_availability = False
+                    booked_unit.save()
+                    self.assigned_unit = booked_unit
+                else:
+                    # Handle the case where the unit is already assigned to another tenant
+                    raise Exception("Selected unit is already assigned to another tenant.")
+            else:
+                # Handle the case where no available unit is found
+                raise Exception("No available unit found with the specified type.")
+
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if self.assigned_unit and not self.assigned_unit.unt_availability:
+            # Set the availability of the unit to 'True'
+            self.assigned_unit.unt_availability = True
+            self.assigned_unit.save()
+
+        # Call the superclass delete method to delete the tenant
+        super().delete(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.tent_name
+
+User.groups.field.remote_field.related_name = 'user_groups'
+User.user_permissions.field.remote_field.related_name = 'user_permissions'
+Tenants.groups.field.remote_field.related_name = 'tenant_groups'
+Tenants.user_permissions.field.remote_field.related_name = 'tenant_permissions'
+
---
+++ coreapp/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    path('home/', views.home, name='home'),
+    path('unit/', views.unit, name='unit'),
+    path('admins/', views.admins, name='admins'),
+    path('creacc/', views.creacc, name='creacc'),
+    path('ad_hom/', views.ad_hom, name='ad_hom'),
+    path('ad_tent/', views.ad_tent, name='ad_tent'),
+    path('tnt_hom/', views.tnt_hom, name='tnt_hom'),
+    path('contact/', views.contact, name='contact'),
+    path('vtour/', views.vtour, name='vtour'),
+    path('amnts/', views.amnts, name='amnts'),
+    path('book/', views.book, name='book'),
+    path('comp/', views.comp, name='comp'),
+    path('req/', views.req, name='req'),
+    path('nav/', views.nav, name='nav'),
+    path('prop/', views.prop, name='prop'),
+    path('rep/', views.rep, name='rep'),
+    path('foot/', views.foot, name='foot'),
+    path('pay/<str:username>/', views.pay, name='pay'),
+    path('delete-tent/<int:tenant_id>/', views.delete_tent, name='delete-tent'),
+    path('delete-unit/<int:unit_id>/', views.delete_unit, name='delete_unit'),
+    path('comp_solv/<int:issue_id>/', views.comp_solv, name='comp_solv'),
+
+    path('logout/', views.user_logout, name='logout'),
+]
