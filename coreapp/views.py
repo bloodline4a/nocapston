33c832ee09c46800f519ef0f64c2528ba0908f37
bloodline4a <pauloroyhernandez@gmail.com>
2023-12-03 12:14:47 +0800

    Update views.py

--- coreapp/views.py
+++ coreapp/views.py
@@ -132,7 +132,7 @@ def creacc(request):
     return render(request, 'creacc.html', {'form': form})
 
 def ad_hom(request):  
-    locale.setlocale(locale.LC_ALL, 'en.UTF-8')
+    locale.setlocale(locale.LC_ALL, 'en_ph.UTF8')
     book = Booked.objects.filter(approval_status='pending').order_by('date')
     tent = Tenants.objects.all().order_by('tent_name')
     prop = Units.objects.all()
